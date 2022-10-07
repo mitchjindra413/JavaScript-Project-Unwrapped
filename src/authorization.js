73533401594daa0b484fc3f5daaf53a5a8acaf3f
Mitchell Jindra <mitchjindra413@gmail.com>
2022-10-06 18:34:51 -0700

    modify auth

--- src/authorization.js
+++ src/authorization.js
@@ -1,24 +1,27 @@
 import Util from './util'
-const client_id = '9b212b049d7743ec9891cd13f67a9b85'; // how do a make a backend to hide this?
-const redirect_uri = ''; //what should i use to test?
 
-let app = express();
+require('dotenv').config();
+const express = require('express');
+const querystring = require('querystring');
+const app = express();
+const port = 8888;
+
+
+const stateKey = 'spotify_auth_state'
 
 
 app.get('/login', (req, res) => {
-    let state = generateRandomString(16)
-    const scope = 'user-read-recently-played user-top-read playlist-modify-public';
+    let state = res.cookie(stateKey, generateRandomString(16))
+    const scope = 'user-read-private user-read-email user-read-recently-played user-top-read playlist-modify-public';
 
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
-            client_id: client_id,
+            client_id: CLIENT_ID,
             response_type: 'code',
-            redirect_uri: redirect_uri,
+            redirect_uri: REDIRECT_URI,
             state: state,
             scope: scope,
-            show_dialog: true,
-            code_chalenge_method: 'S256',
-            code_challenge: sha256(state)
+            show_dialog: true
         })
     )
 })
