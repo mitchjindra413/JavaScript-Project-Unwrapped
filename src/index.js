b3dd07af0422af06e56bf0b3e3a79f2fdb17560c
Mitchell Jindra <mitchjindra413@gmail.com>
2022-10-07 16:48:31 -0700

    setup requests

--- src/index.js
+++ src/index.js
@@ -14,4 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 })
 
+document
+
 
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,32 +1,48 @@
 const key = window.location.hash
 const token = key.slice(key.indexOf("=") + 1, key.indexOf("&"))
 
-function generateTopUserRequestUrl(type, length, time) {
+function generateTopUserRequestUrl(type, length, time) { 
     let url = 'https://api.spotify.com/v1/me/top';
     url += '/' + type;
     url += '?limit=' + length;
-    url += '&time_range=' + time
+    url += '&time_range=' + time;
 
     return url;
 }
+
 // time can be: long_term, medium_term, short_term
 // "https://api.spotify.com/v1/me/shows?offset=0&limit=20\n"
 
-function recieveTopSongRequest(){
-    let type = 'song';
-    let length = '5'; //should I query all 50? or make multiple queries?
+function recieveTopRequest(){
     // let time = some data aspect of the li 
+    const total_data = {};
+    let length = ['short_term', 'medium_term', 'long_term']
+    for(let time in length) {
+        fetch(generateTopUserRequestUrl('song', '50', time), {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        })
+            .then((response) => {
+                console.log(response);
+                // push to total_data
+            });
 
-    fetch('https://api.spotify.com/v1/me/top/tracks', {
-        method: 'GET', 
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        }
-    })
-        .then((response) => {
-            console.log(response);
-        });
+        fetch(generateTopUserRequestUrl('artist', '50', time), {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        })
+            .then((response) => {
+                console.log(response);
+            });
+    }
+    
 }
 
